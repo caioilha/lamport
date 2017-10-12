6829ae53a13f08746e8250822273073420a79797
10108064 <10108064@l3331170.portoalegre.pucrsnet.br>
2017-10-11 22:04:59 -0300

    reading conf file

--- trab.py
+++ trab.py
@@ -1,5 +1,9 @@
 with open('nomecoisa.conf', 'r') as r:
-    lines = r.readLines()
+    lines = r.readlines()
 
-    for (line in lines):
+    for line in lines:
         line = line.split(' ')
+        id_host = line[0]
+        host = line[1]
+        port = line[2]
+        print('id: '+id_host+' host: '+host+' port: '+port)

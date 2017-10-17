a1469a95b796fb30c239d44a9b80fff8d355bfc6
15104291 <15104291@l3331139.portoalegre.pucrsnet.br>
2017-10-16 22:41:37 -0200

    doned

--- trab.py
+++ trab.py
@@ -1,6 +1,42 @@
-import queue
 
-dictop = {}
+import socket
+import threading
+from queue import Queue
+import sys
+import random
+
+class nodo(threading.Thread):
+    def __init__(self, id, host, port, queue):
+        threading.Thread.__init__(self)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((host,port))
+            
+    def run(self):
+        while True:
+            data = self.sock.recvfrom(512)
+            msg = data[0].decode('ascii')
+            rcv = data[1]
+            print(msg +'    '+ str(rcv))
+
+def eventos():
+    for i in range(0,100):
+        if (random.randrange(0,100) > 50):
+            soma = cont_queue.get() + 1 
+            cont_queue.put(soma)
+        else:
+            a = nodos[random.randrange(0,len(nodos))]
+            enviarEvento(nodos[mystuff][0],(a[1],a[2]))
+
+
+def enviarEvento(idsender, data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    soma = cont_queue.get()
+    cont_queue.put(soma)
+    msg = "s " + str(data[0])+' ' + str(soma) +' '+ str(idsender)
+    sock.sendto(msg.encode('ascii'), data)
+
+
+nodos = []
 with open('nomecoisa.conf', 'r') as r:
     lines = r.readlines()
     for line in lines:
@@ -8,6 +44,20 @@ with open('nomecoisa.conf', 'r') as r:
         id_host = line[0]
         host = line[1]
         port = line[2]
-        dictop[id_host] = {'host': host, 'port': port}
+        nodos.append((id_host, host, int(port)))
+
+mystuff = int(sys.argv[1])
+
+id_host = nodos[mystuff][0]
+host = nodos[mystuff][1]
+port = nodos[mystuff][2]
 
 cont_queue = Queue(1)
+cont_queue.put(0)
+nodo = nodo(id_host, host, int(port), cont_queue)
+nodo.start()
+
+eventos()
+
+
+
--- trab_thd.py
+++
@@ -1,8 +0,0 @@
-import socket
-import threading
-
-class nodo(threading.Thread):
-    def __init__(self, host, port):
-        threading.Thread.__init__(self)
-            
-    def run(self):
